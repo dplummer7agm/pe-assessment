c2262c25e576656f764f63eb39fee0397fedfe1b
David Plummer <david@clearlyinnovative.com>
2019-03-10 01:09:14 -0500

    updated the incident service

--- front-end/src/app/incident.service.ts
+++ front-end/src/app/incident.service.ts
@@ -1,8 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class IncidentService {
 
-  constructor() { }
+  constructor(public http:HttpClient) { }
+
+  /** upload the incident file to the server
+   * @param {NgForm} _formData
+   */
+  uploadFile(_formData){
+
+    debugger
+    return new Promise((resolve,reject)=>{
+
+        //send the form data to the server
+       let obs = this.http.post('http://localhost:3000/api/incident/process',_formData)
+        .subscribe((_response)=>{
+          debugger
+          //resolve the promise 
+        resolve(_response);
+        obs.unsubscribe();
+       },(_error)=>{
+
+          //reject the promise
+          debugger
+         reject(_error);
+        obs.unsubscribe();
+       },()=>{
+        debugger
+         obs.unsubscribe();
+       });
+       
+
+      resolve();
+    })
+  }
 
 }
