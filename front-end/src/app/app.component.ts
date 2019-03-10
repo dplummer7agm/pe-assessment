a0f47575d0319b670d42e50cbbcc3c71d4b20f5c
David Plummer <david@clearlyinnovative.com>
2019-03-10 01:20:30 -0500

    defined the form submission event handler

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup} from '@angular/forms';
-
+import { IncidentService } from './incident.service';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +9,19 @@ import { FormGroup} from '@angular/forms';
 })
 export class AppComponent {
 
+  constructor( public incidentService: IncidentService){
+
+  }
   //process form submission
   onSubmit(_formData){
+    this.incidentService.uploadFile(_formData.form.value)
+    .then((_response)=>{
+      debugger
+    })
+    .catch(ex=>{
+      debugger
+     
+    })
   }
 
 }
