43c652ea5558a82fe748d8d38e8c0ae0faa6edbc
David Plummer <david@clearlyinnovative.com>
2019-03-10 13:55:55 -0400

    added more logic to the incident endpoint

--- back-end/routes/incident.js
+++ back-end/routes/incident.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var fse = require('fs-extra');
+var weatherService = require("../lib/weatherService");
 
 //process the incoming incident file
 router.post('/api/incident/process', function (req, res, next) {
+
+(async function(){
 let file = null;
 try{ file = req.files['incidentFile'];}catch(ex){}
 
@@ -26,6 +29,21 @@ try{ file = req.files['incidentFile'];}catch(ex){}
       //else, extract the JSON data from the file object
       let fileJSON = JSON.parse(file.data.toString("utf8"));
       
+      //get the weathe for this incident's location
+      let result = await weatherService.getWeatherByCity({
+        city : fileJSON.address.city,
+        state : fileJSON.address.state,
+        //startDate : new Date(fileJSON.description.event_opened).getTime(), API is returning a 404 error
+        //endDate : new Date(fileJSON.description.event_closed).getTime(), API is returning an 404 error
+      });
+
+      fileJSON.weather = result;
+      if(!result)
+      res.json({
+        success: false,
+        error : "The weather could not be retrieved"
+      })
+      else
       res.json({
         success: true,
         error: false,
@@ -33,12 +51,15 @@ try{ file = req.files['incidentFile'];}catch(ex){}
       })
     }
   } catch (ex) {
+    
     //send an error message back to the client
     res.status(500).json({
       success: false,
       error: `An unexpected error has occurred: ${ex}`
     })
   }
+
+})()
 });
 
 module.exports = router;
