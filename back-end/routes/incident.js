37021ebb6dc54acce336c240166404cfb979d5a8
David Plummer <david@clearlyinnovative.com>
2019-03-09 22:14:09 -0500

    added some more error handling logic

--- back-end/routes/incident.js
+++ back-end/routes/incident.js
@@ -6,13 +6,30 @@ var fse = require('fs-extra');
 router.post('/api/incident/process', function (req, res, next) {
 
   try {
-    
-    res.json({
-      success: true,
-      error: false,
-      response: {}
-    })
+    //if a file hasn't been uploaded, return an error message
+    if (!req.files) {
+      res.json({
+        success: false,
+        error: "You didn't upload an incident file."
+      })
+    } 
+    else if (req.files[Object.keys(req.files)[0]].mimetype != "application/json") {
+      //else, if the wrong file type was upload, return an error message
+      res.json({
+        success: false,
+        error: "Only JSON files are accepted."
+      })
+      
+    } else {
+      //else, attempt to save the JSON file
+      res.json({
+        success: true,
+        error: false,
+        response: {}
+      })
+    }
   } catch (ex) {
+    //send an error message back to the client
     res.status(500).json({
       success: false,
       error: `An unexpected error has occurred: ${ex}`
