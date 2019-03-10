62014a6106245b7f5f53624a491a82961b04878f
David Plummer <david@clearlyinnovative.com>
2019-03-09 22:06:24 -0500

    add some error handling

--- back-end/routes/incident.js
+++ back-end/routes/incident.js
@@ -3,14 +3,21 @@ var router = express.Router();
 var fse = require('fs-extra');
 
 //process the incoming incident file
-router.post('/api/incident/process', function(req, res, next) {
-  
-  
-  res.json({
-    success : true,
-    error : false,
-    response : {}
-  })
+router.post('/api/incident/process', function (req, res, next) {
+
+  try {
+    
+    res.json({
+      success: true,
+      error: false,
+      response: {}
+    })
+  } catch (ex) {
+    res.status(500).json({
+      success: false,
+      error: `An unexpected error has occurred: ${ex}`
+    })
+  }
 });
 
 module.exports = router;
